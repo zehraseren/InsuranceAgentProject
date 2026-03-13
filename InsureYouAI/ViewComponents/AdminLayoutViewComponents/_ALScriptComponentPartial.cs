6c1ab569668a861301b4616270b8b6844e52561e
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-13 10:56:59 +0300

    feat(admin layout): create Script view component

---
+++ InsureYouAI/ViewComponents/AdminLayoutViewComponents/_ALScriptComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.AdminLayoutViewComponents;
+
+public class _ALScriptComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
