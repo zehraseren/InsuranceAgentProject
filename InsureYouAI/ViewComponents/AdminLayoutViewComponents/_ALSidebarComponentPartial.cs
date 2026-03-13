f6a7c0995c8339aa595fe33ebc304fa76ea33507
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-13 11:22:27 +0300

    feat(admin layout): create Sidebar view component

---
+++ InsureYouAI/ViewComponents/AdminLayoutViewComponents/_ALSidebarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.AdminLayoutViewComponents;
+
+public class _ALSidebarComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
