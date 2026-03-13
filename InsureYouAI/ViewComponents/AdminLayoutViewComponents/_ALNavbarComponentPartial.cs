c2f8a832d620e474f6a49e27576cd9bbf6ac5aab
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-13 10:58:15 +0300

    feat(admin layout): create Navbar view component

---
+++ InsureYouAI/ViewComponents/AdminLayoutViewComponents/_ALNavbarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.AdminLayoutViewComponents;
+
+public class _ALNavbarComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
