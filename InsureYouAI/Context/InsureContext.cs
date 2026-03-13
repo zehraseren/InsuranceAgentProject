603fd7ff1b2b6dee5c84e9a8bdd22290983e8fa0
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-13 09:57:34 +0300

    feat: add ApplicationDbContext

---
+++ InsureYouAI/Context/InsureContext.cs
@@ -0,0 +1,23 @@
+﻿using InsureYouAI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsureYouAI.Context;
+
+public class InsureContext : DbContext
+{
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=Zehra;initial catalog=InsureDb;integrated security=true;trust server certificate=true");
+    }
+
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<AboutItem> AboutItems { get; set; }
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<PricingPlan> PricingPlans { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+    public DbSet<TrailerVideo> TrailerVideos { get; set; }
+}
