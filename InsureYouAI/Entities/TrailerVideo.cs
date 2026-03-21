59d7aaf22bfa81fa00dc19f7c0e5372c3490ca54
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-21 09:30:54 +0300

    feat(database): add VideoUrl property to TrailerVideo

--- InsureYouAI/Entities/TrailerVideo.cs
+++ InsureYouAI/Entities/TrailerVideo.cs
@@ -5,4 +5,5 @@ public class TrailerVideo
     public int TrailerVideoId { get; set; }
     public string Title { get; set; }
     public string CoverImageUrl { get; set; }
+    public string VideoUrl { get; set; }
 }
---
+++ InsureYouAI/Migrations/20260321030543_AddVideoUrlPropertyToTrailerVideo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsureYouAI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVideoUrlPropertyToTrailerVideo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoUrl",
+                table: "TrailerVideos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoUrl",
+                table: "TrailerVideos");
+        }
+    }
+}
