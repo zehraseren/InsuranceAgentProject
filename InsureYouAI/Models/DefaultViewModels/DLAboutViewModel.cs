48940c6a2bbbcff69f3cea56706f143cebb242af
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 22:12:36 +0300

    feat(default layout): create About view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLAboutViewModel.cs
@@ -0,0 +1,11 @@
+﻿using InsureYouAI.Entities;
+
+namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLAboutViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public List<string> Details { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLAboutComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLAboutComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLAboutComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var about = _context.Abouts.FirstOrDefault();
+        var model = new Models.DefaultViewModels.DLAboutViewModel
+        {
+            Title = about.Title,
+            Description = about.Description,
+            ImageUrl = about.ImageUrl,
+            Details = _context.AboutItems.Select(ai => ai.Detail).ToList()
+        };
+
+        return View(model);
+    }
+}
