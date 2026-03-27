2d8065e198b512f7e7f3dbfb3c5fc6fbd4212c4e
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 13:20:35 +0300

    feat(default layout): create Head view component

---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLHeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLHeadComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
