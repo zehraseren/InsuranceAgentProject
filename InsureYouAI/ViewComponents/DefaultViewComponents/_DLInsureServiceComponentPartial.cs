28274f44c8a0e83490848d7d5673d834258b8895
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 22:42:58 +0300

    feat(default layout): create InsureService view component

---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLInsureServiceComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLInsureServiceComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
