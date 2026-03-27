3769c769cb7efbf61912427bb1c627031eca85b9
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 11:35:05 +0300

    feat(register): implement CRUD operations

---
+++ InsureYouAI/Controllers/RegisterController.cs
@@ -0,0 +1,55 @@
+﻿using InsureYouAI.Dtos.RegisterUserDtos;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers;
+
+public class RegisterController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public RegisterController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public IActionResult CreateUser()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(CreateRegisterUserDto crudto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(crudto);
+        }
+
+        AppUser appUser = new AppUser
+        {
+            Name = crudto.Name,
+            Surname = crudto.Surname,
+            UserName = crudto.Username,
+            Email = crudto.Email,
+            ImageUrl = "Url",
+            Description = "Açıklama"
+        };
+
+        var result = await _userManager.CreateAsync(appUser, crudto.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(crudto);
+        }
+
+        return RedirectToAction("UserList");
+    }
+}
---
+++ InsureYouAI/Dtos/RegisterUserDtos/CreateRegisterUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsureYouAI.Dtos.RegisterUserDtos;
+
+public class CreateRegisterUserDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    [Required(ErrorMessage ="Kullanım şartlarını kabul etmeniz gerekmektedir!")]
+    [Range(typeof(bool), "true", "true", ErrorMessage = "Kullanım şartlarını kabul etmeniz gerekmektedir!")]
+    public bool AcceptTerms { get; set; }
+}
