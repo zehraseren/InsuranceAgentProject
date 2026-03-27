9f4c2ca7fd3530ee10cc1b078de329a07116a2f9
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 15:19:43 +0300

    feat(default layout): create Slider view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLSliderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLSliderViewModel
+{
+    public int SliderId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLSliderComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLSliderComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLSliderComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var sliders = _context.Sliders
+            .Select(s => new DLSliderViewModel
+            {
+                SliderId = s.SliderId,
+                Title = s.Title,
+                Description = s.Description,
+                ImageUrl = s.ImageUrl,
+            }).ToList();
+        return View(sliders);
+    }
+}
