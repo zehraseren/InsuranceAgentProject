e612446ed13efca75e3eec29f401165b1ee71e03
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 14:26:37 +0300

    feat(default layout): create MobileMenu view component

---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLMobileMenuComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLMobileMenuComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLMobileMenuComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var contact = _context.Contacts
+            .Select(c => new DLHeaderContactViewModel
+            {
+                Email = c.Email,
+                Phone = c.Phone
+            }).FirstOrDefault();
+
+        return View(contact ?? new DLHeaderContactViewModel());
+    }
+}
