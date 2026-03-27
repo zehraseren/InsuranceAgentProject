95e673d5dd08500be863a9d210b0ecaf19a31997
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-27 23:01:20 +0300

    feat(default layout): create TrailerVideo view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLTrailerVideoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLTrailerVideoViewModel
+{
+    public string Title { get; set; }
+    public string CoverImageUrl { get; set; }
+    public string VideoUrl { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLTrailerVideoComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLTrailerVideoComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLTrailerVideoComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var trailerVideo = _context.TrailerVideos
+            .Select(tv => new DLTrailerVideoViewModel
+            {
+                Title = tv.Title,
+                CoverImageUrl = tv.CoverImageUrl,
+                VideoUrl = tv.VideoUrl,
+            }).ToList();
+
+        return View(trailerVideo);
+    }
+}
