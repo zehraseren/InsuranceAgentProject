4ee604416e9e7caf469ced89007c09abe9720e7f
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-31 13:48:30 +0300

    feat(default layout): create Counter view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLCounterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLCounterViewModel
+{
+    public int CategoryCount { get; set; }
+    public int ServiceCount { get; set; }
+    public int UserCount { get; set; }
+    public int ArticleCount { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLCounterComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLCounterComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLCounterComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var model = new DLCounterViewModel
+        {
+            CategoryCount = _context.Categories.Count(),
+            ServiceCount = _context.Services.Count(),
+            UserCount = _context.Users.Count(),
+            ArticleCount = _context.Articles.Count()
+        };
+
+        return View(model);
+    }
+}
