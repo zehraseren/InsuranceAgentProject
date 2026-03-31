a2da5061158b4cae3e3ca2e01a6486538e5145fb
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-31 18:48:12 +0300

    feat(default layout): create Gallery view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLGalleryViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLGalleryViewModel
+{
+    public string Title { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLGalleryComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLGalleryComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLGalleryComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var galleries = _context.Galleries
+            .Select(g => new DLGalleryViewModel
+            {
+                Title = g.Title,
+                ImageUrl = g.ImageUrl
+            }).ToList();
+
+        return View(galleries);
+    }
+}
