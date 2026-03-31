1beeb9d3ab4099c119449d40fc4b76bc325d11f4
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-31 16:15:02 +0300

    feat(default layout): create Testimonial view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLTestimonialViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLTestimonialViewModel
+{
+    public string NameSurname { get; set; }
+    public string CommentDetail { get; set; }
+    public string Title { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLTestimonialComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLTestimonialComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLTestimonialComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var testimonials = _context.Testimonials
+            .Select(t => new DLTestimonialViewModel
+            {
+                NameSurname = t.NameSurname,
+                Title = t.Title,
+                CommentDetail = t.CommentDetail,
+                ImageUrl = t.ImageUrl,
+            }).ToList();
+
+        return View(testimonials);
+    }
+}
