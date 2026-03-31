3f6638aaa9a7cb87505005c46a04962ed15777f4
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-31 13:34:39 +0300

    feat(default layout): create Service view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLServiceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLServiceViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string? IconUrl { get; set; }
+    public string? ImageUrl { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLServiceComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLServiceComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLServiceComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var services = _context.Services
+            .Select(s => new DLServiceViewModel
+            {
+                Title = s.Title,
+                Description = s.Description,
+                IconUrl = s.IconUrl,
+                ImageUrl = s.ImageUrl
+            }).ToList();
+        return View(services);
+    }
+}
