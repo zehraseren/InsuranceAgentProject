449c012541ee33c5c28eb6c58a9848ca44f8c532
Zehra Seren <fatmazehraseren@gmail.com>
2026-03-31 15:29:06 +0300

    feat(default layout): create PricingPlan view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLPricingPlanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using InsureYouAI.Entities;
+
+namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLPricingPlanViewModel
+{
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public bool IsFeature { get; set; }
+    public List<PricingPlanItem> PricingPlanItems { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLPricingPlanComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLPricingPlanComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLPricingPlanComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var pricingPlans = _context.PricingPlans
+            .Where(pp => pp.IsFeature)
+            .Select(pp=> new DLPricingPlanViewModel
+            {
+                Title = pp.Title,
+                Price = pp.Price,
+                IsFeature = pp.IsFeature,
+                PricingPlanItems = pp.PricingPlanItems.ToList(),
+            }).ToList();
+
+        return View(pricingPlans);
+    }
+}
