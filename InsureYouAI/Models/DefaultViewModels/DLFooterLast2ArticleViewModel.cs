86265f65bf87eecd452663a9913bcefdf0a4d7ca
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-01 23:05:20 +0300

    feat(default layout): create FooterLast2Article view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLFooterLast2ArticleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLFooterLast2ArticleViewModel
+{
+    public string Title { get; set; }
+    public string CoverImageUrl { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLFooterLast2ArticleComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLFooterLast2ArticleComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLFooterLast2ArticleComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var articles = _context.Articles.
+            Select(a => new DLFooterLast2ArticleViewModel
+            {
+                Title = a.Title,
+                CoverImageUrl = a.CoverImageUrl,
+                CreatedDate = a.CreatedTime
+            }).Skip(3).Take(2).ToList();
+
+        return View(articles);
+    }
+}
