bd32e50560411f9e60a9069aff2563488b87204b
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-01 16:56:27 +0300

    feat(default layout): create Footer view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLFooterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLFooterViewModel
+{
+    public string Description { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLFooterComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLFooterComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLFooterComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var contact = _context.Contacts
+            .Select(c => new DLFooterViewModel
+            {
+                Description = c.Description,
+                Email = c.Email,
+                Phone = c.Phone,
+                Address = c.Address
+            }).FirstOrDefault();
+
+        return View(contact);
+    }
+}
