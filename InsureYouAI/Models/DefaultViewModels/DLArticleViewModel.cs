d119cc58d4ada5f2a43d9db19f742a9a57a9e2fe
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-01 14:50:00 +0300

    feat(default layout): create Last3Article view component

---
+++ InsureYouAI/Models/DefaultViewModels/DLArticleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InsureYouAI.Entities;
+
+namespace InsureYouAI.Models.DefaultViewModels;
+
+public class DLArticleViewModel
+{
+    public string Title { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public string Content { get; set; }
+    public string CoverImageUrl { get; set; }
+    public string MainCoverImageUrl { get; set; }
+    public string CategoryName { get; set; }
+    public List<Comment> Comments { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DLLast3ArticleComponentPartial.cs
@@ -0,0 +1,37 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InsureYouAI.Models.DefaultViewModels;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DLLast3ArticleComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DLLast3ArticleComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var articles = _context.Articles
+            .Include(a => a.Category)
+            .OrderByDescending(a => a.ArticleId)
+            .Take(3)
+            .Select(a => new DLArticleViewModel
+            {
+                Title = a.Title,
+                CreatedTime = a.CreatedTime,
+                Content = a.Content,
+                CoverImageUrl = a.CoverImageUrl,
+                MainCoverImageUrl = a.MainCoverImageUrl,
+                CategoryName = a.Category.CategoryName,
+                Comments = a.Comments.ToList()
+            })
+            .ToList();
+
+        return View(articles);
+    }
+}
