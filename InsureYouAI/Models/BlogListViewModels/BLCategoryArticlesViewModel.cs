9b7aceb3bef8a034f7121b30ef1b35b8321ae728
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-03 20:38:41 +0300

    feat(blog): add categories component

---
+++ InsureYouAI/Models/BlogListViewModels/BLCategoryArticlesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InsureYouAI.Models.BlogListViewModels;
+
+public class BLCategoryArticlesViewModel
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public int ArticleCount { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/BlogListViewComponents/_BLCategoriesComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.BlogListViewModels;
+
+namespace InsureYouAI.ViewComponents.BlogListViewComponents;
+
+public class _BLCategoriesComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _BLCategoriesComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var categoryArticles = _context.Categories
+            .Select(ca => new BLCategoryArticlesViewModel
+            {
+                CategoryId = ca.CategoryId,
+                CategoryName = ca.CategoryName,
+                ArticleCount = ca.Articles.Count()
+            }).ToList();
+
+        return View(categoryArticles);
+    }
+}
