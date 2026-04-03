bfc3dbd9b854c87382f1c44899845d2ecd6d3f63
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-03 23:53:18 +0300

    feat(blog): add top article component

---
+++ InsureYouAI/Models/BlogListViewModels/BLTopArticleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InsureYouAI.Models.BlogListViewModels;
+
+public class BLTopArticleViewModel
+{
+    public int ArticleId { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedTime { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/BlogListViewComponents/_BLTopArticlesComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.BlogListViewModels;
+
+namespace InsureYouAI.ViewComponents.BlogListViewComponents;
+
+public class _BLTopArticlesComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _BLTopArticlesComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var topArticles = _context.Articles
+            .OrderByDescending(a => a.CreatedTime)
+            .Take(3)
+            .Select(a => new BLTopArticleViewModel
+            {
+                ArticleId = a.ArticleId,
+                Title = a.Title,
+                CreatedTime = a.CreatedTime
+            }).ToList();
+
+        return View(topArticles);
+    }
+}
