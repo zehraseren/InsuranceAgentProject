6f7727cd1afbc2e49a2a32ca52836afecc8f6454
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-03 19:13:21 +0300

    feat(blog): add blog list component

---
+++ InsureYouAI/Models/BlogListViewModels/BLBlogListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using InsureYouAI.Entities;
+
+namespace InsureYouAI.Models.BlogListViewModels;
+
+public class BLBlogListViewModel
+{
+    public int ArticleId { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedDate{ get; set; }
+    public string Content { get; set; }
+    public string CoverImageUrl { get; set; }
+    public string MainCoverImageUrl { get; set; }
+    public string CategoryName { get; set; }
+    public string Author { get; set; }
+    public int CommentCount { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/BlogListViewComponents/_BLBlogListComponentPartial.cs
@@ -0,0 +1,39 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InsureYouAI.Models.BlogListViewModels;
+
+namespace InsureYouAI.ViewComponents.BlogListViewComponents;
+
+public class _BLBlogListComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _BLBlogListComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var articles = _context.Articles
+             .Include(a => a.Category)
+             .Include(u => u.AppUser)
+             .Include(c => c.Comments)
+             .Select(a => new BLBlogListViewModel
+             {
+                 ArticleId = a.ArticleId,
+                 Title = a.Title,
+                 CreatedDate = a.CreatedTime,
+                 Content = a.Content,
+                 CoverImageUrl = a.CoverImageUrl,
+                 MainCoverImageUrl = a.MainCoverImageUrl,
+                 CategoryName = a.Category.CategoryName,
+                 Author = a.AppUser.Name + " " + a.AppUser.Surname,
+                 CommentCount = a.Comments.Count()
+             })
+             .ToList();
+
+        return View(articles);
+    }
+}
