b52118ac3af2a4066aeb44eb0244a7a53502c274
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-03 18:05:11 +0300

    feat(blog): add breadcrumb component

---
+++ InsureYouAI/ViewComponents/BlogListViewComponents/_BLBreadcrumbComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.BlogListViewComponents;
+
+public class _BLBreadcrumbComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
