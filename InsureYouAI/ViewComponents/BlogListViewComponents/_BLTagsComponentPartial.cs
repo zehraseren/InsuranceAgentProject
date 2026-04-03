f1caddef47e1dccac02adfbdcf767f1c074bbb55
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-04 00:04:14 +0300

    feat(blog): add tags component

---
+++ InsureYouAI/ViewComponents/BlogListViewComponents/_BLTagsComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.BlogListViewComponents;
+
+public class _BLTagsComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
