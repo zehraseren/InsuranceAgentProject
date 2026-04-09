cc0a99b7ab6161289fb6862799fce9cdadf985a4
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-09 22:22:39 +0300

    fix(blog-detail): display comments in comment list component

---
+++ InsureYouAI/Models/BlogDetailViewModels/BDCommentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InsureYouAI.Models.BlogDetailViewModels;
+
+public class BDCommentViewModel
+{
+    public string Author { get; set; }
+    public DateTime CommentDate { get; set; }
+    public string CommentDetail { get; set; }
+    public string AuthorImageUrl { get; set; }
+    public int CommentCount { get; set; }
+}
--- InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDCommentListComponentPartial.cs
+++ InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDCommentListComponentPartial.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InsureYouAI.Models.BlogDetailViewModels;
 
 namespace InsureYouAI.ViewComponents.BlogDetailViewComponents;
 
 public class _BDCommentListComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly InsureContext _context;
+
+    public _BDCommentListComponentPartial(InsureContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke(int articleId)
+    {
+        var approvedStatus = "Yorum Onaylandı";
+        var commentCount = _context.Comments.Count(x => x.ArticleId == articleId && x.CommentStatus == approvedStatus);
+
+        var comments = _context.Comments
+            .Include(a => a.AppUser)
+            .Where(c => c.ArticleId == articleId && c.CommentStatus == approvedStatus)
+            .Select(c => new BDCommentViewModel
+            {
+                Author = c.AppUser.Name + " " + c.AppUser.Surname,
+                CommentDate = c.CommentDate,
+                CommentDetail = c.CommentDetail,
+                AuthorImageUrl = c.AppUser.ImageUrl,
+                CommentCount = commentCount
+            }).ToList();
+
+        return View(comments);
     }
 }
