b0fb95a8dd4998e02695b504b023db5a188be28e
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-09 20:14:38 +0300

    feat(blog-detail): add comment list component

---
+++ InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDCommentListComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.BlogDetailViewComponents;
+
+public class _BDCommentListComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
