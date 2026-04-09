653e9275640fbdda0ea302122f8d8e36522be5b4
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-09 20:12:50 +0300

    feat(blog-detail): add next and previous post component

---
+++ InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDNextAndPreviousPostComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.BlogDetailViewComponents;
+
+public class _BDNextAndPreviousPostComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
