81621c4a58d51715e152e954d458dac47c2abac3
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-14 06:52:34 +0300

    feat(dashboard): add widgets component

---
+++ InsureYouAI/Models/DashboardViewModels/DSHWidgetsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InsureYouAI.Models.DashboardViewModels;
+
+public class DSHWidgetsViewModel
+{
+    public int ArticleCount { get; set; }
+    public int CategoryCount { get; set; }
+    public int CommentCount { get; set; }
+    public int UserCount { get; set; }
+    public string Rate1 { get; set; }
+    public string Rate2 { get; set; }
+    public string Rate3 { get; set; }
+    public string Rate4 { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DSHWidgetsComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DashboardViewModels;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents;
+
+public class _DSHWidgetsComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DSHWidgetsComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var random = new Random();
+
+        var model = new DSHWidgetsViewModel
+        {
+            ArticleCount = _context.Articles.Count(),
+            CategoryCount = _context.Categories.Count(),
+            CommentCount = _context.Comments.Count(),
+            UserCount = _context.Users.Count(),
+            Rate1 = $"{random.Next(1, 30)}.{random.Next(0, 10)}",
+            Rate2 = $"{random.Next(1, 30)}.{random.Next(0, 10)}",
+            Rate3 = $"{random.Next(1, 30)}.{random.Next(0, 10)}",
+            Rate4 = $"{random.Next(1, 30)}.{random.Next(0, 10)}"
+        };
+
+        return View(model);
+    }
+}
