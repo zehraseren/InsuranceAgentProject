8e6fdaedba6e57f9cf1f02102a7fe8b340600b4e
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-14 06:39:48 +0300

    feat(dashboard): add scripts component

---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DSHScriptsComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents;
+
+public class _DSHScriptsComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
