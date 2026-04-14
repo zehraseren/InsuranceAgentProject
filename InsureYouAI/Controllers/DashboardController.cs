d9225baa40edc4a16663dafd55951f9bab802fb9
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-14 05:59:08 +0300

    feat(dashboard): initialize dashboard module

---
+++ InsureYouAI/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
