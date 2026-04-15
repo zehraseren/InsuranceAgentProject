f6c99a2adf55da48469ec40b2b3f257bb51efab1
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-15 03:35:55 +0300

    feat(dashboard): add secondary chart component

---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DSHSecondaryChartComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents;
+
+public class _DSHSecondaryChartComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
