e7289f07b8f67048eb99866524159f631b6ada33
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-16 17:45:24 +0300

    feat(dashboard): add radial chart view component

---
+++ InsureYouAI/Models/DashboardViewModels/DSHRadialChartViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace InsureYouAI.Models.DashboardViewModels;
+
+public class DSHRadialChartViewModel
+{
+    public int TotalPolicies { get; set; }
+    public int ActivePolicies { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DSHRadialChartComponentPartial.cs
@@ -0,0 +1,26 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DashboardViewModels;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents;
+
+public class _DSHRadialChartComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DSHRadialChartComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var model = new DSHRadialChartViewModel
+        {
+            TotalPolicies = _context.Policies.Count(),
+            ActivePolicies = _context.Policies.Count(p => p.Status == "Active")
+        };
+
+        return View(model);
+    }
+}
