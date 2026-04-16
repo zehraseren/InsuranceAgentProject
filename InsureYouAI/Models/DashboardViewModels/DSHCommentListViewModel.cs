6ee54d08d27c618eb67d369c4973ce34a3358438
Zehra Seren <fatmazehraseren@gmail.com>
2026-04-16 04:39:47 +0300

    feat(dashboard): add comment list component

---
+++ InsureYouAI/Models/DashboardViewModels/DSHCommentListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.DashboardViewModels;
+
+public class DSHCommentListViewModel
+{
+    public string NameSurname { get; set; }
+    public string CommentDetail { get; set; }
+    public string ImageUrl { get; set; }
+    public DateTime CommentDate { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DSHCommentListComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DashboardViewModels;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents;
+
+public class _DSHCommentListComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DSHCommentListComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var comments = _context.Comments
+            .OrderByDescending(c => c.CommentId)
+            .Take(7)
+            .Select(c => new DSHCommentListViewModel
+            {
+                NameSurname = c.AppUser.Name + " " + c.AppUser.Surname,
+                CommentDetail = c.CommentDetail,
+                ImageUrl = c.AppUser.ImageUrl,
+                CommentDate = c.CommentDate
+            }).ToList();
+
+        return View(comments);
+    }
+}
