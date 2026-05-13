60b311450e476c7292ba532481b3497bdc82b256
Zehra Seren <fatmazehraseren@gmail.com>
2026-05-14 02:57:34 +0300

    feat(blog-detail): display author information in blog detail

---
+++ InsureYouAI/Models/BlogDetailViewModels/BDAuthorInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsureYouAI.Models.BlogDetailViewModels;
+
+public class BDAuthorInfoViewModel
+{
+    public string NameSurname { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string? Title { get; set; }
+}
--- InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDAuthorInfoComponentPartial.cs
+++ InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDAuthorInfoComponentPartial.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.BlogDetailViewModels;
 
 namespace InsureYouAI.ViewComponents.BlogDetailViewComponents;
 
 public class _BDAuthorInfoComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly InsureContext _context;
+
+    public _BDAuthorInfoComponentPartial(InsureContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke(int id)
+    {
+        var authorInfo = _context.Articles
+            .Where(a => a.ArticleId == id)
+            .Select(au => new BDAuthorInfoViewModel
+            {
+                NameSurname = au.AppUser.Name + " " + au.AppUser.Surname,
+                Description = au.AppUser.Description,
+                Title = au.AppUser.Title,
+                ImageUrl = au.AppUser.ImageUrl
+            }).FirstOrDefault();
+
+        if (authorInfo == null) return Content("");
+
+        return View(authorInfo);
     }
 }
