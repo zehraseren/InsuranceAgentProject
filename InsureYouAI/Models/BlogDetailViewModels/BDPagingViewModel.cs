350110fc61d06d7b8f369098bf0985363958b93c
Zehra Seren <fatmazehraseren@gmail.com>
2026-05-14 02:32:51 +0300

    feat(blog-detail): implement next and previous post functionality

---
+++ InsureYouAI/Models/BlogDetailViewModels/BDPagingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InsureYouAI.Models.BlogDetailViewModels;
+
+public class BDPagingViewModel
+{
+    public int CurrentId { get; set; }
+    public int? PreviousId { get; set; }
+    public string PreviousTitle { get; set; }
+    public int? NextId { get; set; }
+    public string NextTitle { get; set; }
+}
--- InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDNextAndPreviousPostComponentPartial.cs
+++ InsureYouAI/ViewComponents/BlogDetailViewComponents/_BDNextAndPreviousPostComponentPartial.cs
@@ -1,11 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.BlogDetailViewModels;
 
 namespace InsureYouAI.ViewComponents.BlogDetailViewComponents;
 
 public class _BDNextAndPreviousPostComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly InsureContext _context;
+
+    public _BDNextAndPreviousPostComponentPartial(InsureContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke(int id)
+    {
+        var article = _context.Articles.FirstOrDefault(x => x.ArticleId == id);
+
+        var previous = _context.Articles
+            .Where(p => p.ArticleId < id)
+            .OrderByDescending(p => p.ArticleId)
+            .Select(p => new { p.ArticleId, p.Title })
+            .FirstOrDefault();
+
+        var next = _context.Articles
+            .Where(n => n.ArticleId > id)
+            .OrderBy(n => n.ArticleId)
+            .Select(n => new { n.ArticleId, n.Title })
+            .FirstOrDefault();
+
+        var model = new BDPagingViewModel
+        {
+            CurrentId = id,
+            PreviousId = previous?.ArticleId,
+            PreviousTitle = previous?.Title,
+            NextId = next?.ArticleId,
+            NextTitle = next?.Title
+        };
+
+        return View(model);
     }
 }
