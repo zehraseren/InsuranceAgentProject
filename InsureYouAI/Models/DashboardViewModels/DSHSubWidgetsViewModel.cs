2597a1673995ac6be9d10820dca41e09303d56c9
Zehra Seren <fatmazehraseren@gmail.com>
2026-06-02 23:48:21 +0300

    feat(dashboard): add sub widgets component

---
+++ InsureYouAI/Models/DashboardViewModels/DSHSubWidgetsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InsureYouAI.Models.DashboardViewModels;
+
+public class DSHSubWidgetsViewModel
+{
+    public int TotalCategoryCount { get; set; }
+    public int TotalArticleCount { get; set; }
+    public int TotalPoliciesCount { get; set; }
+    public int TotalPoliciesByThisMonthCount { get; set; }
+    public int TotalCommentCount { get; set; }
+    public int TotalUserCount { get; set; }
+    public decimal AvgPolicyAmount { get; set; }
+    public decimal LastRevenueAmount { get; set; }
+}
---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DSHSubWidgetsComponentPartial.cs
@@ -0,0 +1,41 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using InsureYouAI.Models.DashboardViewModels;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents;
+
+public class _DSHSubWidgetsComponentPartial : ViewComponent
+{
+    private readonly InsureContext _context;
+
+    public _DSHSubWidgetsComponentPartial(InsureContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var currentDate = DateTime.Now;
+        var startOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+        var startOfNextMonth = startOfMonth.AddMonths(1);
+
+        var model = new DSHSubWidgetsViewModel
+        {
+            TotalCategoryCount = _context.Categories.Count(),
+            TotalArticleCount = _context.Articles.Count(),
+            TotalPoliciesCount = _context.Policies.Count(),
+            TotalPoliciesByThisMonthCount = _context.Policies
+                .Where(p => p.CreatedDate >= startOfMonth && p.CreatedDate < startOfNextMonth)
+                .Count(),
+            TotalCommentCount = _context.Comments.Count(),
+            TotalUserCount = _context.Users.Count(),
+            AvgPolicyAmount = _context.Policies.Average(a => a.PremiumAmount),
+            LastRevenueAmount = _context.Revenues
+                .OrderByDescending(r => r.RevenueId)
+                .Select(r => r.Amount)
+                .FirstOrDefault()
+        };
+
+        return View(model);
+    }
+}
