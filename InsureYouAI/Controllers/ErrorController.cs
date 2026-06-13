601d6d691dbe6588d179a10717ad3c601a58295d
Zehra Seren <fatmazehraseren@gmail.com>
2026-06-13 19:20:54 +0300

    feat(error): add custom 404 error page

---
+++ InsureYouAI/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers;
+
+public class ErrorController : Controller
+{
+    [Route("Error/404")]
+    public IActionResult Page404()
+    {
+        return View();
+    }
+}
--- InsureYouAI/Program.cs
+++ InsureYouAI/Program.cs
@@ -31,6 +31,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/Error/500");
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
